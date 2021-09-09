4f2196dd1541dcb3c424e67bbc0a78d72e74560f
SUJITH C S <43125517+Code-Suji@users.noreply.github.com>
2021-09-09 12:56:13 +0530

    Create DynamicMethodDispatch.java

---
+++ DynamicMethodDispatch.java
@@ -0,0 +1,24 @@
+class A{
+    public void show(){
+        System.out.println(("In A"));
+    }
+}
+class B extends A{
+    public void show(){
+        System.out.println(("In B"));
+    }
+}
+class C extends A{
+    public void show(){
+        System.out.println(("In C"));
+    }
+}
+public class DynamicMethodDispatch {
+    public static void main(String[] args) {
+        A obj = new B();
+        obj.show();
+        obj = new C();
+        obj.show();
+
+    }
+}
